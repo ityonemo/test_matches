7b773810e358b835be09d36d5b9284b93af95936
ityonemo <isaac.yonemoto@gmail.com>
2020-03-21 13:32:09 -0700

    blanko

---
+++ lib/test_matches.ex
@@ -0,0 +1,18 @@
+defmodule TestMatches do
+  @moduledoc """
+  Documentation for `TestMatches`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TestMatches.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/test_matches_test.exs
@@ -0,0 +1,8 @@
+defmodule TestMatchesTest do
+  use ExUnit.Case
+  doctest TestMatches
+
+  test "greets the world" do
+    assert TestMatches.hello() == :world
+  end
+end
